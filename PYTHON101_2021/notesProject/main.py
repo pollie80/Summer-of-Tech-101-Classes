08a56f12c67a4e94016be58de9187d62728c03ea
Tian Welgemoed <tian.welgemoed@gmail.com>
2021-05-18 19:29:46 +1200

    Notes for python

---
+++ PYTHON101_2021/notesProject/main.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+capitals = {
+    # keys   :  values
+    "Germany": "Berlin"
+}
+
+def print_class(value):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'{value} is a ', type(value))  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #classes
+    print_class('PyCharm')
+    print_class(0)
+    print_class([0])
+    print_class(input("Enter something: "))
+
+    #dictionary
+    print(capitals['Germany'])
+
+    #loop through list
+    cities = []
+    for city in cities:
+        print(city)
+
+    #
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
